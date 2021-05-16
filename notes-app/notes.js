fd33e321a9a1dbb8ecbdae41ba3efe29797c018b
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-16 11:51:56 -0700

    using debugger

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -15,6 +15,8 @@ const addNote = (title, body) => {
     // to our new json object
     const duplicateNote = notes.find((note) => note.title === title)
 
+    debugger
+
     if (!duplicateNote) {
         notes.push({
             title: title,
