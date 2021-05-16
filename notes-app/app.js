5a86941a5ad42937a94a6883f3cd3bb915d4fc3f
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-15 22:36:23 -0700

    using yargs part1

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,40 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
+// customize yargs version
+yargs.version('1.1.0')
 
-console.log(notes)
-console.log(chalk.red('Success!'))
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add new note',
+    handler: function () {
+        console.log('adding new note')
+    }
+})
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    handler: function () {
+        console.log('Removing a note')
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'reads note',
+    handler: function () {
+        console.log('reading note')
+    }
+})
+yargs.command({
+    command: 'list',
+    describe: 'lists all notes',
+    handler: function () {
+        console.log('Listing all Notes')
+    }
+})
+
+
+console.log(process.argv)
+console.log(yargs.argv)
