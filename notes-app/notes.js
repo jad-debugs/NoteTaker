adfe0b00f731311c5e682d20e88be9518a254002
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-15 23:44:46 -0700

    note add feature completed

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,51 @@
+const fs = require('fs')
+
 const getNotes = function() {
     return 'Your Notes...'
 }
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+    const notes = loadNotes()
+
+    // filter is like a for each loop for each json object
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    }
+    else {
+        console.log('Note title already taken!')
+    }
+
+}
+
+const saveNotes = function (notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = function() {
+    // error when adding note
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch (e) {
+        return []
+    }
+
+    
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
