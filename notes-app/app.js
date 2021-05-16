caaee856f6ecd8af90dde7d4f02f25fbd509ed55
Jad Isaac <67189765+jad-debugs@users.noreply.github.com>
2021-05-15 22:07:39 -0700

    using chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
-console.log(notes)
 
-console.log(validator.isURL('https://mead.i'))
+console.log(notes)
+console.log(chalk.red('Success!'))
